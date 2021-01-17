4ded179df275813d8729e640411bdd61ffa78860
afroman09 <shinp09@gmail.com>
2021-01-17 16:32:06 +0900

    reactの導入

--- main.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-
-const startbtn = document.getElementById('start-btn');
-
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <div class="container">
+        <h1>Want to Vintage Clothes?</h1>
+        <div class="wrapper">
+            <ul class="box">
+            <li id="img">画像を表示</li>
+            <li id="text">テキストを表示</li>
+            </ul>
+            <input type="button" value="START" id="start-btn"></input>
+        </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
