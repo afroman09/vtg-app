fcc706477571689987519c4a87ef07d324a6f6fc
afroman09 <shinp09@gmail.com>
2021-01-17 16:42:49 +0900

    修正

--- components/Home.js
+++ components/Home.js
@@ -1,17 +1,18 @@
-import "./App.css";
+import React from "react";
 
-function App() {
+export default function Home() {
   return (
-    <div className="App">
+    <div>
       <div class="container">
         <h1>Want to Vintage Clothes?</h1>
         <div class="wrapper">
-
+          <ul class="box">
+            <li id="img">画像を表示</li>
+            <li id="text">テキストを表示</li>
+          </ul>
           <input type="button" value="START" id="start-btn"></input>
         </div>
       </div>
     </div>
   );
 }
-
-export default App;
