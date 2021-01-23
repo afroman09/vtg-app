5c628517f03e5047a4c8cbba09f3f98125d5e583
afroman09 <shinp09@gmail.com>
2021-01-23 22:33:30 +0900

    test

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React/*, {useState} */from "react";
 import { withRouter } from "react-router";
-import img1 from "./1.jpg";
-import img2 from "./img/2.jpg";
-import img3 from "./img/3.jpg";
+import img1 from "../assets/img/utumi.jpeg";
+import img2 from "../assets/img/komaba.jpeg";
 
 const Main = () => {
+
+// const [testImg, testImgs] = useState("");
+
   function getImage() {
-    let clothes = [img1, img2, img3];
+    let clothes = [img1, img2];
     const result = Math.floor(Math.random() * clothes.length);
     const resalt = clothes[result];
     return resalt;
   }
 
+  // const testimg = getImage();
+
   return (
     <React.Fragment>
       <div class="wrapper">
-        <div id="img">{getImage()}</div>
+        <img id="testImg" alt="" value = {getImage()}></img>
         <button id="start-btn" onclick={getImage()}>
           START
         </button>
