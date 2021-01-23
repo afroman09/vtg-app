9faa507e3abd75a134626eb76019e8835fd02788
afroman09 <shinp09@gmail.com>
2021-01-23 22:55:04 +0900

    imgの表示

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,25 +1,25 @@
-import React/*, {useState} */from "react";
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
 import { withRouter } from "react-router";
 import img1 from "../assets/img/utumi.jpeg";
 import img2 from "../assets/img/komaba.jpeg";
 
 const Main = () => {
 
-// const [testImg, testImgs] = useState("");
 
   function getImage() {
     let clothes = [img1, img2];
     const result = Math.floor(Math.random() * clothes.length);
     const resalt = clothes[result];
-    return resalt;
+    return resalt
   }
 
-  // const testimg = getImage();
+  const testimg = getImage();
 
   return (
     <React.Fragment>
       <div class="wrapper">
-        <img id="testImg" alt="" value = {getImage()}></img>
+        <img id="testImg" src={testimg}></img>
         <button id="start-btn" onclick={getImage()}>
           START
         </button>
